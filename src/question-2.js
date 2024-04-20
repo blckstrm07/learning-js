f091d17679bf88aa6d71077f580bacb696580356
blckstrm07 <115084102+blckstrm07@users.noreply.github.com>
2024-04-20 14:11:24 +0800

    Add files via upload

---
+++ src/question-2.js
@@ -0,0 +1,8 @@
+function numbers(min, max) {
+	for (let i = min; i <= max; i++) {
+		console.log(numbers)
+	}
+}
+
+let min = prompt("Enter Lower Limit:2");
+let max = prompt("Enter upper limit:10");
